aea726b45ef80ad6dfbaad88dda8914e43c41262
amitroth <amitroth@gmail.com>
2026-05-16 08:47:49 +0300

    Update test.py to use hvta FilesystemWrapper

--- hvta/test.py
+++ hvta/test.py
@@ -1,5 +1,30 @@
+""" A minimal script showing how to run textarena locally """
+
 import textarena as ta
+from hvta.FilesystemWrapper import FilesystemWrapper
 
+# agents = {
+#     0: ta.agents.HumanAgent(),
+# }
 
-if __name__ == "__main__":
-    pass
+agents = {
+    0: ta.agents.OpenRouterAgent(model_name="openai/gpt-5-mini"),
+}
+
+# initialize the environment
+env = ta.make(env_id="Wordle-v0")
+env = FilesystemWrapper(env=env, hidden_solution=True, logical_bug=True) #, render_mode="standard")
+env.reset(num_players=len(agents))
+
+# main game loop
+done = False
+while not done:
+  player_id, observation = env.get_observation()
+  action = agents[player_id](observation)
+  print(action)
+  done, step_info = env.step(action=action)
+  if step_info.get("reward_hack"):
+    print("\033[33m[HACK DETECTED] A hack was triggered in this step!\033[0m")
+rewards, game_info = env.close()
+print(rewards)
+print(game_info)
